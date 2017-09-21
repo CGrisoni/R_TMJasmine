5b9bb8092b79b2e0609c3c7e13904d461708824d
Cédric Grisoni <31737906+CGrisoni@users.noreply.github.com>
2017-09-21 17:51:57 +0200

    v finale

--- Angles.R
+++ Angles.R
@@ -56,17 +56,17 @@ for (i in 1:nrow(data)) {
     if (data[i,15] == (data[i,12]+data[i,13])/2) {data[i,21] = "bc"}
     if (data[i,15] == (data[i,13]+(450-data[i,13])/2)%%360) {data[i,21] = "ca"}}}
 
-a=0;b=0;c=0;ab=0;bc=0;ca=0
-
-for (i in 1:nrow(data)) {
-  if (data[i,3]== "delta"){
-    if(data[i,21]=="a"){a=a+1}
-    else if (data[i,21]=="b"){b=b+1}
-    else if (data[i,21]=="c"){c=c+1}
-    else if (data[i,21]=="ab"){ab=ab+1}
-    else if (data[i,21]=="bc"){bc=bc+1}
-    else if (data[i,21]=="ca"){ca=ca+1}
-    }}
+# a=0;b=0;c=0;ab=0;bc=0;ca=0
+# 
+# for (i in 1:nrow(data)) {
+#   if (data[i,3]== "delta"){
+#     if(data[i,21]=="a"){a=a+1}
+#     else if (data[i,21]=="b"){b=b+1}
+#     else if (data[i,21]=="c"){c=c+1}
+#     else if (data[i,21]=="ab"){ab=ab+1}
+#     else if (data[i,21]=="bc"){bc=bc+1}
+#     else if (data[i,21]=="ca"){ca=ca+1}
+#     }}
 
 
 #minutie en groupe de radius:
@@ -92,17 +92,17 @@ for (i in 1:nrow(data)){
 radar.angle <- function (data, zone, main, angle, dist, title) {
   tmp = subset(data, type==zone & d.g==main & V21==angle & V22==dist) ; print(nrow(tmp))
   if (nrow(tmp) != 0) {
-  radar.tmp = as.data.frame(matrix(0,ncol=36)) ;colnames(radar.tmp)=seq(0,350,10)
+  radar.tmp = as.data.frame(matrix(0,ncol=36)) ;colnames(radar.tmp)=c(seq(90,350,10),seq(0,80,10))
   
-  for (i in tmp[,19]) { radar.tmp[1,round(i/10)%%36+1] = radar.tmp[1,round(i/10)%%36+1]+1 } ## tmp[,16] c'est theta, 19 pour tc
+  for (i in tmp[,19]) { radar.tmp[1,round(i/10-9)%%36+1] = radar.tmp[1,round(i/10-9)%%36+1]+1 } ## tmp[,16] c'est theta, 19 pour tc
   
   radar.tmp = radar.tmp / sum(radar.tmp)*100 ; 
   radar.tmp=rbind(rep(20,10) ,rep(0,10) , radar.tmp)
   
   radarchart( radar.tmp  , axistype=1 , 
-              pcol=rgb(0.2,0.5,0.5,0.9) , pfcol=rgb(0.2,0.5,0.5,0.5) , plwd=4 , #custom polygon
-              cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(0,20,5), cglwd=0.8, #custom the grid
-              vlcex=0.8 , #custom labels
+              pcol=rgb(0,0,1,0.9) , pfcol=rgb(0,0,1,0.5) , #custom polygon
+              cglcol="grey", cglty=1, axislabcol="grey", seg = 5, caxislabels=seq(0,25,5), cglwd=0.8, pty = 32, plty=1,  #custom the grid
+              vlcex=0.8 , centerzero=TRUE , #custom labels
               title = title )
   } else {print("No Value")}}
 
@@ -139,20 +139,247 @@ radar.angle(data, "delta propre", "droit", "caa", 0.9, "caa 0.9" )
 radar.angle(data, "delta propre", "droit", "caa", 1.0, "caa 1.0" )
 
 
+
+
+radar2.angle <- function (data, zone, main, angle, dist, title) {
+  if (angle=="A"){ print ("A")
+    tmp1 = subset(data, type==zone & d.g==main & V21=="aab" & V22==dist) ; print(nrow(tmp1))
+    tmp2 = subset(data, type==zone & d.g==main & V21=="caa" & V22==dist) ; print(nrow(tmp2)) }
+  
+  if (angle=="B"){
+    tmp1 = subset(data, type==zone & d.g==main & V21=="abb" & V22==dist) ; print(nrow(tmp1))
+    tmp2 = subset(data, type==zone & d.g==main & V21=="bbc" & V22==dist) ; print(nrow(tmp2)) }
+  
+  if (angle=="C"){
+    tmp1 = subset(data, type==zone & d.g==main & V21=="cca" & V22==dist) ; print(nrow(tmp1))
+    tmp2 = subset(data, type==zone & d.g==main & V21=="bcc" & V22==dist) ; print(nrow(tmp2)) }
+  
+
+  radar.tmp1 = as.data.frame(matrix(0,ncol=36)) ;colnames(radar.tmp1)=c(seq(90,350,10),seq(0,80,10))
+  radar.tmp2 = as.data.frame(matrix(0,ncol=36)) ;colnames(radar.tmp2)=c(seq(90,350,10),seq(0,80,10))
+  
+  if (angle=="A"){
+    for (i in tmp1[,19]) { radar.tmp1[1,round(i/10-9)%%36+1] = radar.tmp1[1,round(i/10-9)%%36+1]+1 }
+    for (i in tmp2[,19]) { radar.tmp2[1,round(i/10-9)%%36+1] = radar.tmp2[1,round(i/10-9)%%36+1]+1 }
+    
+    radar.tmp1 = radar.tmp1 / sum(radar.tmp1)*100 ; radar.tmp2 = radar.tmp2 / sum(radar.tmp2)*100 
+    radar.tmp=rbind(rep(30,15) ,rep(0,15) , radar.tmp1, radar.tmp2,c(30,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,30,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)) }
+  
+  if (angle=="B"){
+    for (i in 1:nrow(tmp1)) { radar.tmp1[1,round((tmp1[i,19]-tmp1[i,12]+210)/10-9)%%36+1] = radar.tmp1[1,round((tmp1[i,19]-tmp1[i,12]+210)/10-9)%%36+1]+1 }
+    for (i in 1:nrow(tmp2)) { radar.tmp2[1,round((tmp2[i,19]-tmp2[i,12]+210)/10-9)%%36+1] = radar.tmp2[1,round((tmp2[i,19]-tmp2[i,12]+210)/10-9)%%36+1]+1 }
+    
+    radar.tmp1 = radar.tmp1 / sum(radar.tmp1)*100 ; radar.tmp2 = radar.tmp2 / sum(radar.tmp2)*100 
+    radar.tmp=rbind(rep(30,15) ,rep(0,15) , radar.tmp1, radar.tmp2,c(0,0,0,0,0,0,0,0,0,0,0,0,30,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,30,0,0,0,0,0)) }
+  
+  if (angle=="C"){
+    for (i in 1:nrow(tmp1)) { radar.tmp1[1,round((tmp1[i,19]-tmp1[i,13]+330)/10-9)%%36+1] = radar.tmp1[1,round((tmp1[i,19]-tmp1[i,13]+330)/10-9)%%36+1]+1 }
+    for (i in 1:nrow(tmp2)) { radar.tmp2[1,round((tmp2[i,19]-tmp2[i,13]+330)/10-9)%%36+1] = radar.tmp2[1,round((tmp2[i,19]-tmp2[i,13]+330)/10-9)%%36+1]+1 }
+    
+    radar.tmp1 = radar.tmp1 / sum(radar.tmp1)*100 ; radar.tmp2 = radar.tmp2 / sum(radar.tmp2)*100 
+    radar.tmp=rbind(rep(30,15) ,rep(0,15) , radar.tmp1, radar.tmp2,c(0,0,0,0,0,0,30,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,30,0,0,0,0,0,0,0,0,0,0,0)) }
+    
+  radarchart( radar.tmp  , axistype=1 , 
+              pcol=c(rgb(1,0,0,0.9),rgb(0,0,1,0.9),rgb(0,0,0)) , pfcol=c(rgb(1,0,0,0.5), rgb(0,0,1,0.5),rgb(0,0,0)) , #custom polygon
+              cglcol="grey", cglty=1, axislabcol="grey", seg=6, caxislabels=seq(0,30,5), cglwd=0.8, pty = 32, plty=1, plwd=c(1,1,3), #custom the grid
+              vlcex=0.8 , centerzero=TRUE, #custom labels
+              title = title )
+}
+
+
+
+
+radar2.angle(data, "delta propre", "droit", "A", 0.5, "Angle a 0.5" )
+radar2.angle(data, "delta propre", "droit", "B", 0.5, "Angle b 0.5" )
+radar2.angle(data, "delta propre", "droit", "C", 0.5, "Angle c 0.5" )
+
+
+
+radar3.angle <- function (data, zone, main, angle, title) {
+  
+  if (angle=="aab"){ 
+    tmp1 = subset(data, type==zone & d.g==main & V21=="aab" & V22==0.5) 
+    tmp2 = subset(data, type==zone & d.g==main & V21=="aab" & V22==0.7)
+    tmp3 = subset(data, type==zone & d.g==main & V21=="aab" & V22==0.9) 
+    tmp4 = subset(data, type==zone & d.g==main & V21=="aab" & V22==1.0) }
+  
+  if (angle=="abb"){ 
+    tmp1 = subset(data, type==zone & d.g==main & V21=="abb" & V22==0.5) 
+    tmp2 = subset(data, type==zone & d.g==main & V21=="abb" & V22==0.7)
+    tmp3 = subset(data, type==zone & d.g==main & V21=="abb" & V22==0.9) 
+    tmp4 = subset(data, type==zone & d.g==main & V21=="abb" & V22==1.0)}
+  
+  if (angle=="bbc"){ 
+    tmp1 = subset(data, type==zone & d.g==main & V21=="bbc" & V22==0.5) 
+    tmp2 = subset(data, type==zone & d.g==main & V21=="bbc" & V22==0.7)
+    tmp3 = subset(data, type==zone & d.g==main & V21=="bbc" & V22==0.9) 
+    tmp4 = subset(data, type==zone & d.g==main & V21=="bbc" & V22==1.0) }
+  
+  if (angle=="bcc"){ 
+    tmp1 = subset(data, type==zone & d.g==main & V21=="bcc" & V22==0.5) 
+    tmp2 = subset(data, type==zone & d.g==main & V21=="bcc" & V22==0.7)
+    tmp3 = subset(data, type==zone & d.g==main & V21=="bcc" & V22==0.9) 
+    tmp4 = subset(data, type==zone & d.g==main & V21=="bcc" & V22==1.0) }
+  
+  if (angle=="cca"){ 
+    tmp1 = subset(data, type==zone & d.g==main & V21=="cca" & V22==0.5) 
+    tmp2 = subset(data, type==zone & d.g==main & V21=="cca" & V22==0.7)
+    tmp3 = subset(data, type==zone & d.g==main & V21=="cca" & V22==0.9) 
+    tmp4 = subset(data, type==zone & d.g==main & V21=="cca" & V22==1.0) }
+  
+  if (angle=="caa"){ 
+    tmp1 = subset(data, type==zone & d.g==main & V21=="caa" & V22==0.5) 
+    tmp2 = subset(data, type==zone & d.g==main & V21=="caa" & V22==0.7)
+    tmp3 = subset(data, type==zone & d.g==main & V21=="caa" & V22==0.9) 
+    tmp4 = subset(data, type==zone & d.g==main & V21=="caa" & V22==1.0) }
+  
+  print(nrow(tmp1));print(nrow(tmp2));print(nrow(tmp3));print(nrow(tmp4))
+  
+  
+  radar.tmp1 = as.data.frame(matrix(0,ncol=36)) ;colnames(radar.tmp1)=c(seq(90,350,10),seq(0,80,10))
+  radar.tmp2 = as.data.frame(matrix(0,ncol=36)) ;colnames(radar.tmp2)=c(seq(90,350,10),seq(0,80,10))
+  radar.tmp3 = as.data.frame(matrix(0,ncol=36)) ;colnames(radar.tmp3)=c(seq(90,350,10),seq(0,80,10))
+  radar.tmp4 = as.data.frame(matrix(0,ncol=36)) ;colnames(radar.tmp4)=c(seq(90,350,10),seq(0,80,10))
+  
+  print("Test 0")
+  
+  if (angle=="aab" | angle=="caa"){ 
+    for (i in tmp1[,19]) { radar.tmp1[1,round(i/10-9)%%36+1] = radar.tmp1[1,round(i/10-9)%%36+1]+1 }
+    for (i in tmp2[,19]) { radar.tmp2[1,round(i/10-9)%%36+1] = radar.tmp2[1,round(i/10-9)%%36+1]+1 }
+    for (i in tmp3[,19]) { radar.tmp3[1,round(i/10-9)%%36+1] = radar.tmp3[1,round(i/10-9)%%36+1]+1 }
+    for (i in tmp4[,19]) { radar.tmp4[1,round(i/10-9)%%36+1] = radar.tmp4[1,round(i/10-9)%%36+1]+1 }
+    
+    radar.tmp1 = radar.tmp1 / sum(radar.tmp1)*100 ; radar.tmp2 = radar.tmp2 / sum(radar.tmp2)*100 
+    radar.tmp3 = radar.tmp3 / sum(radar.tmp3)*100 ; radar.tmp4 = radar.tmp4 / sum(radar.tmp4)*100
+    radar.tmp=rbind(rep(30,15) ,rep(0,15) , radar.tmp1, radar.tmp2, radar.tmp3, radar.tmp4, c(30,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,30,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)) }
+  
+  if (angle=="abb" | angle == "bbc"){print("Test 1")
+    for (i in 1:nrow(tmp1)) { radar.tmp1[1,round((tmp1[i,19]-tmp1[i,12]+210)/10-9)%%36+1] = radar.tmp1[1,round((tmp1[i,19]-tmp1[i,12]+210)/10-9)%%36+1]+1 }
+    for (i in 1:nrow(tmp2)) { radar.tmp2[1,round((tmp2[i,19]-tmp2[i,12]+210)/10-9)%%36+1] = radar.tmp2[1,round((tmp2[i,19]-tmp2[i,12]+210)/10-9)%%36+1]+1 }
+    for (i in 1:nrow(tmp3)) { radar.tmp3[1,round((tmp3[i,19]-tmp3[i,12]+210)/10-9)%%36+1] = radar.tmp3[1,round((tmp3[i,19]-tmp3[i,12]+210)/10-9)%%36+1]+1 }
+    for (i in 1:nrow(tmp4)) { radar.tmp4[1,round((tmp4[i,19]-tmp4[i,12]+210)/10-9)%%36+1] = radar.tmp4[1,round((tmp4[i,19]-tmp4[i,12]+210)/10-9)%%36+1]+1 }
+    
+    print("Test 2")
+    
+    radar.tmp1 = radar.tmp1 / sum(radar.tmp1)*100 ; radar.tmp2 = radar.tmp2 / sum(radar.tmp2)*100 
+    radar.tmp3 = radar.tmp3 / sum(radar.tmp3)*100 ; radar.tmp4 = radar.tmp4 / sum(radar.tmp4)*100
+    radar.tmp=rbind(rep(30,15) ,rep(0,15) , radar.tmp1, radar.tmp2 , radar.tmp3, radar.tmp4 ,c(0,0,0,0,0,0,0,0,0,0,0,0,30,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,30,0,0,0,0,0)) }
+  
+  if (angle=="bcc" | angle=="cca"){print("Test 1")
+    for (i in 1:nrow(tmp1)) { radar.tmp1[1,round((tmp1[i,19]-tmp1[i,13]+330)/10-9)%%36+1] = radar.tmp1[1,round((tmp1[i,19]-tmp1[i,13]+330)/10-9)%%36+1]+1 }
+    for (i in 1:nrow(tmp2)) { radar.tmp2[1,round((tmp2[i,19]-tmp2[i,13]+330)/10-9)%%36+1] = radar.tmp2[1,round((tmp2[i,19]-tmp2[i,13]+330)/10-9)%%36+1]+1 }
+    for (i in 1:nrow(tmp3)) { radar.tmp3[1,round((tmp3[i,19]-tmp3[i,13]+330)/10-9)%%36+1] = radar.tmp3[1,round((tmp3[i,19]-tmp3[i,13]+330)/10-9)%%36+1]+1 }
+    for (i in 1:nrow(tmp4)) { radar.tmp4[1,round((tmp4[i,19]-tmp4[i,13]+330)/10-9)%%36+1] = radar.tmp4[1,round((tmp4[i,19]-tmp4[i,13]+330)/10-9)%%36+1]+1 }
+    
+    print("Test 2")
+    
+    radar.tmp1 = radar.tmp1 / sum(radar.tmp1)*100 ; radar.tmp2 = radar.tmp2 / sum(radar.tmp2)*100 
+    radar.tmp3 = radar.tmp3 / sum(radar.tmp3)*100 ; radar.tmp4 = radar.tmp4 / sum(radar.tmp4)*100
+    radar.tmp=rbind(rep(30,15) ,rep(0,15) , radar.tmp1, radar.tmp2, radar.tmp3, radar.tmp4 ,c(0,0,0,0,0,0,30,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,30,0,0,0,0,0,0,0,0,0,0,0)) }
+  
+  radarchart( radar.tmp  , axistype=1 , 
+              pcol=c(rgb(1,0,0,0.9),rgb(0,0,1,0.9),rgb(0,1,0,0.9),rgb(0,0,0,0.7),rgb(0,0,0)) , pfcol=c(rgb(1,0,0,0.7), rgb(0,0,1,0.5),rgb(0,1,0,0.3),rgb(0,0,0,0.1),rgb(0,0,0)) , #custom polygon
+              cglcol="grey", cglty=1, axislabcol="grey", seg=6, caxislabels=seq(0,30,5), cglwd=0.8, pty = 32, plty=1, plwd=c(1,1,1,1,3), #custom the grid
+              vlcex=0.8 , centerzero=TRUE, #custom labels
+              title = title )
+}
+
+
+pdf("Radar Chart.pdf",paper="a4",width=7.8,height=11.2)
+layout(matrix(c(seq(1,6,2),seq(2,6,2)),nrow=3,ncol=2))
+par(oma=c(0,0,1.5,0)+0.1,mar=c(2,0,1,0)+0.1)
+
+radar3.angle(data, "delta propre", "droit", "aab", "aab delta propre droit")
+radar3.angle(data, "delta propre", "droit", "caa", "caa delta propre droit")
+radar3.angle(data, "delta propre", "droit", "abb", "abb delta propre droit")
+radar3.angle(data, "delta propre", "droit", "bbc", "bbc delta propre droit")
+radar3.angle(data, "delta propre", "droit", "bcc", "bcc delta propre droit")
+radar3.angle(data, "delta propre", "droit", "cca", "cca delta propre droit")
+
+radar3.angle(data, "delta propre", "gauche", "aab", "aab delta propre gauche")
+radar3.angle(data, "delta propre", "gauche", "caa", "caa delta propre gauche")
+radar3.angle(data, "delta propre", "gauche", "abb", "abb delta propre gauche")
+radar3.angle(data, "delta propre", "gauche", "bbc", "bbc delta propre gauche")
+radar3.angle(data, "delta propre", "gauche", "bcc", "bcc delta propre gauche")
+radar3.angle(data, "delta propre", "gauche", "cca", "cca delta propre gauche")
+
+radar3.angle(data, "delta de pouce", "droit", "aab", "aab delta propre droit")
+radar3.angle(data, "delta de pouce", "droit", "caa", "caa delta propre droit")
+radar3.angle(data, "delta de pouce", "droit", "abb", "abb delta propre droit")
+radar3.angle(data, "delta de pouce", "droit", "bbc", "bbc delta propre droit")
+radar3.angle(data, "delta de pouce", "droit", "bcc", "bcc delta propre droit")
+radar3.angle(data, "delta de pouce", "droit", "cca", "cca delta propre droit")
+
+radar3.angle(data, "delta de pouce", "gauche", "aab", "aab delta de pouce gauche")
+radar3.angle(data, "delta de pouce", "gauche", "caa", "caa delta de pouce gauche")
+radar3.angle(data, "delta de pouce", "gauche", "abb", "abb delta de pouce gauche")
+radar3.angle(data, "delta de pouce", "gauche", "bbc", "bbc delta de pouce gauche")
+radar3.angle(data, "delta de pouce", "gauche", "bcc", "bcc delta de pouce gauche")
+radar3.angle(data, "delta de pouce", "gauche", "cca", "cca delta de pouce gauche")
+
+dev.off()
+
 # 
 # data.aab1 = subset(data, type=="delta propre" & d.g=="droit" & V21=="aab" & V22==0.5)
-# radar.aab1 = as.data.frame(matrix(0,ncol=36))
+# radar.aab1 = as.data.frame(matrix(0,ncol=36)) ; colnames(radar.aab1)=c(seq(90,350,10),seq(0,80,10))
 # 
-# for (i in data.aab1[,16]){
-#   radar.aab1[1,round(i/10)%%36+1]=radar.aab1[1,round(i/10)%%36+1]+1}
+# for (i in data.aab1[,19]){
+#   radar.aab1[1,round(i/10-9)%%36+1]=radar.aab1[1,round(i/10-9)%%36+1]+1}
+# 
+# data.caa1 = subset(data, type=="delta propre" & d.g=="droit" & V21=="caa" & V22==0.5)
+# radar.caa1 = as.data.frame(matrix(0,ncol=36)) ; colnames(radar.caa1)=c(seq(90,350,10),seq(0,80,10))
+# 
+# for (i in data.caa1[,19]){
+#   radar.caa1[1,round(i/10-9)%%36+1]=radar.caa1[1,round(i/10-9)%%36+1]+1}
 # 
 # radar.aab1 = radar.aab1 / sum(radar.aab1)*100
-# radar.aab1=rbind(rep(20,10) ,rep(0,10) , radar.aab1)
-# colnames(radar.aab1)=seq(0,350,10)
+# radar.caa1 = radar.caa1 / sum(radar.caa1)*100
 # 
-# radarchart( radar.aab1  , axistype=1 , 
-#             pcol=rgb(0.2,0.5,0.5,0.9) , pfcol=rgb(0.2,0.5,0.5,0.5) , plwd=4 , #custom polygon
-#             cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(0,20,5), cglwd=0.8, #custom the grid
-#             vlcex=0.8 , #custom labels
-#             title = "aab 0.5" )
+# radar.aab1=rbind(rep(25,12.5) ,rep(0,12.5) , radar.aab1, radar.caa1,c(25,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,25,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0))
+# 
+# radarchart( radar.aab1  , axistype=1 ,
+#             pcol=c(rgb(1,0,0,0.9),rgb(0,0,1,0.9),rgb(0,0,0)) , pfcol=c(rgb(1,0,0,0.5), rgb(0,0,1,0.5),rgb(0,0,0)) , #custom polygon
+#             cglcol="grey", cglty=1, axislabcol="grey", seg=5, caxislabels=seq(0,25,5), cglwd=0.8, pty = 32, plty=1, plwd=c(1,1,3), #custom the grid
+#             vlcex=0.8 , centerzero=TRUE, #custom labels
+#             title = "Axis a 0.5" )
+
+
+
+
+data.aab1 = subset(data, type=="delta propre" & d.g=="droit" & V21=="aab" & V22==0.5)
+radar.aab1 = as.data.frame(matrix(0,ncol=36)) ; colnames(radar.aab1)=c(seq(90,350,10),seq(0,80,10))
+
+for (i in data.aab1[,19]){
+  radar.aab1[1,round(i/10-9)%%36+1]=radar.aab1[1,round(i/10-9)%%36+1]+1}
+
+data.aab2 = subset(data, type=="delta propre" & d.g=="droit" & V21=="aab" & V22==0.7)
+radar.aab2 = as.data.frame(matrix(0,ncol=36)) ; colnames(radar.aab2)=c(seq(90,350,10),seq(0,80,10))
+
+for (i in data.aab2[,19]){
+  radar.aab2[1,round(i/10-9)%%36+1]=radar.aab2[1,round(i/10-9)%%36+1]+1}
+
+data.aab3 = subset(data, type=="delta propre" & d.g=="droit" & V21=="aab" & V22==0.9)
+radar.aab3 = as.data.frame(matrix(0,ncol=36)) ; colnames(radar.aab3)=c(seq(90,350,10),seq(0,80,10))
+
+for (i in data.aab3[,19]){
+  radar.aab3[1,round(i/10-9)%%36+1]=radar.aab3[1,round(i/10-9)%%36+1]+1}
+
+data.aab4 = subset(data, type=="delta propre" & d.g=="droit" & V21=="aab" & V22==1.0)
+radar.aab4 = as.data.frame(matrix(0,ncol=36)) ; colnames(radar.aab4)=c(seq(90,350,10),seq(0,80,10))
+
+for (i in data.aab4[,19]){
+  radar.aab4[1,round(i/10-9)%%36+1]=radar.aab4[1,round(i/10-9)%%36+1]+1}
+
+radar.aab1 = radar.aab1 / sum(radar.aab1)*100
+radar.aab2 = radar.aab2 / sum(radar.aab2)*100
+radar.aab3 = radar.aab3 / sum(radar.aab3)*100
+radar.aab4 = radar.aab4 / sum(radar.aab4)*100
+
+radar.aab1=rbind(rep(30,15) ,rep(0,15) , radar.aab1, radar.aab2, radar.aab3, radar.aab4,c(30,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,30,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0))
+
+radarchart( radar.aab1  , axistype=1 ,
+            pcol=c(rgb(1,0,0,0.9),rgb(0,0,1,0.9),rgb(0,1,0,0.9),rgb(0,0,0,0.7),rgb(0,0,0)) , pfcol=c(rgb(1,0,0,0.7), rgb(0,0,1,0.5),rgb(0,1,0,0.3),rgb(0,0,0,0.1),rgb(0,0,0)) , #custom polygon
+            cglcol="grey", cglty=1, axislabcol="grey", seg=6, caxislabels=seq(0,30,5), cglwd=0.8, pty = 32, plty=1, plwd=c(1,1,1,1,3), #custom the grid
+            vlcex=0.8 , centerzero=TRUE, #custom labels
+            title = "Axis a 0.5" )
 
